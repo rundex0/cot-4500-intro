ffc3a7b78d024befd4a473f59c52c2bc8e9abbef
rundex0 <69157728+rundex0@users.noreply.github.com>
2023-01-18 20:43:53 -0500

    Update intro_to_python.py

--- src/main/intro_to_python.py
+++ src/main/intro_to_python.py
@@ -1,1 +1,9 @@
-text
+import numpy
+a = numpy.matrix([[1, 3, 3], [3, 1, 3], [3, 3, 1]])
+print(a)
+
+a = numpy.matrix([[1, 3, 3], [3, 1, 3], [3, 3, 1]])
+print("\n",a)
+
+a = numpy.delete(a, 2, 1)
+print("\n",a)
